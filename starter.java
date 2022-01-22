4566a351e4cd6682943ca70f02b04fe962b7f7ae
polyqubit <polyumbrial@gmail.com>
2022-01-21 19:21:17 -0800

    starting input

--- starter.java
+++ starter.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class starter {
 	static void p(String s) {System.out.print(s);}
 	public static void main(String[] args) {
-		Grid g = new Grid(10,10);
+		Scanner sc = new Scanner(System.in);
+		p("");
+		Grid g = new Grid(30,30);
 		g.constructGrid();
-		p("Success\n");
+		p("\nGrid constructed successfully\n");
 		int[] start = {0,0};
 		int[] end = {g.width-1,g.height-1}; //redo to let user define
 		ArrayList<Node> path = Path.returnPath(g, new Node(start,0,0), end);
